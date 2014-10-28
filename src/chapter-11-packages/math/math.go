b0453f3a244db67752c2471a3e0e553308102467
tuss4 <tuss4dbfn@gmail.com>
2014-10-28 14:18:35 -0500

    Added test structures.

--- src/chapter-11-packages/math/math.go
+++ src/chapter-11-packages/math/math.go
@@ -4,6 +4,9 @@ package math
 // Finds the average of a series of numbers
 func Average(xs []float64) float64 {
     total := float64(0)
+    if len(xs) == 0 {
+        return total
+    }
     for _, x := range xs {
         total += x
     }
@@ -12,6 +15,9 @@ func Average(xs []float64) float64 {
 
 // Finds the smallest number in a series of numbers
 func Min(xs []float64) float64 {
+    if len(xs) == 0 {
+        return float64(0)
+    }
     i := xs[0]
     for _, x := range xs {
         if i > x {
@@ -23,6 +29,9 @@ func Min(xs []float64) float64 {
 
 // Finds the greatest number in a series of numbers
 func Max(xs []float64) float64 {
+    if len(xs) == 0 {
+        return float64(0)
+    }
     i := float64(0)
     for _, x := range xs {
         if i < x {
--- src/chapter-11-packages/math/math_test.go
+++ src/chapter-11-packages/math/math_test.go
@@ -3,29 +3,79 @@ package math
 
 import "testing"
 
+type testpair struct {
+    values []float64
+    average float64
+}
+
+var tests = []testpair{
+    { []float64{1,2}, 1.5},
+    { []float64{1,1,1,1,1,1}, 1},
+    { []float64{-1,1}, 0},
+    { []float64{}, 0},
+}
+
+type hiTestPair struct {
+    values []float64
+    max float64
+}
+
+var hiTests = []hiTestPair{
+    { []float64{1,2, 54}, 54},
+    { []float64{0.000000001, 0.00001}, 0.00001},
+    { []float64{-1,1}, 1},
+    { []float64{}, 0},
+}
+
+type loTestPair struct {
+    values []float64
+    min float64
+}
+
+var loTests = []loTestPair{
+    { []float64{1,2, 54}, 1},
+    { []float64{0.000000001, 0.00001}, 0.000000001},
+    { []float64{-1,1}, -1},
+    { []float64{}, 0},
+}
 // Test Average
 func TestAverage(t *testing.T) {
-    var v float64
-    v = Average([]float64{1,2})
-    if v != 1.5 {
-        t.Error("Expected 1.5, got ", v)
+    for _, pair := range tests {
+        v := Average(pair.values)
+        if v != pair.average {
+            t.Error(
+                "For", pair.values,
+                "expected", pair.average,
+                "got", v,
+            )
+        }
     }
 }
 
 // Test Max
 func TestMax(t *testing.T) {
-    var v float64
-    v = Max([]float64{17,34,1})
-    if v != 34 {
-        t.Error("Expected 34, got ", v)
+    for _, pair := range hiTests {
+        v := Max(pair.values)
+        if v != pair.max {
+            t.Error(
+                "For", pair.values,
+                "expected", pair.max,
+                "got", v,
+            )
+        }
     }
 }
 
 //Test Min
 func TestMin(t *testing.T) {
-    var v float64
-    v = Min([]float64{17,34,1})
-    if v != 1 {
-        t.Error("Expected 1, got ", v)
+    for _, pair := range loTests {
+        v := Min(pair.values)
+        if v != pair.min {
+            t.Error(
+                "For", pair.values,
+                "expected", pair.min,
+                "got", v,
+            )
+        }
     }
 }
