0ef36ac54452414df166a7e620267555a2a006f1
tuss4 <tuss4dbfn@gmail.com>
2014-10-28 08:55:21 -0500

    Finished the structs and interfaces problems.

--- go_book.go
+++ go_book.go
@@ -224,6 +224,8 @@ func main() {
     rect := Rectangle{0, 0, 10, 10}
     fmt.Println(rect.area())
     fmt.Println(totalArea(&cir, &rect))
+    fmt.Println(rect.perimeter())
+    fmt.Println(cir.perimeter())
 }
 
 func setMyName(name *string) *string {
@@ -385,6 +387,10 @@ func (c *Circle) area() float64 {
     return math.Pi * c.r*c.r
 }
 
+func (c *Circle) perimeter() float64 {
+    return 2 * math.Pi * c.r
+}
+
 type Rectangle struct {
     x1, y1, x2, y2 float64
 }
@@ -399,6 +405,12 @@ func (r *Rectangle) area() float64 {
     l := distance(r.x1, r.y1, r.x1, r.y2)
     w := distance(r.x1, r.y1, r.x2, r.y1)
     return l * w
+}
+
+func (r *Rectangle) perimeter() float64 {
+    l := distance(r.x1, r.y1, r.x1, r.y2)
+    w := distance(r.x1, r.y1, r.x2, r.y1)
+    return 2*(l + w)
 }
 
 type Person struct {
@@ -416,6 +428,7 @@ type Android struct {
 
 type Shape interface {
     area() float64
+    perimeter() float64
 }
 
 func totalArea(shapes ...Shape) float64 {
